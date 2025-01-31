e6b66b7deab69980483197abb96ce5c636c7820e
ai-moin-musa <moin.musa@icreativetechnolabs.com>
2025-01-31 18:44:42 +0530

    [modify] modify the structure of files and folders

--- Library_management_module_AM/ak_library_management/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# import all models or register all models here
-from . import book
-from . import category
-from . import member
--- Library_management_module_Hiren_sir/ak_library_management/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-#import all models from the models folder
-from . import models
